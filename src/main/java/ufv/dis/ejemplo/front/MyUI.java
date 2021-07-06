0b909de717841e6f36d9a62616d37726a1e34a4b
Enrique Ruiz <Ruiz@DeHaagseHogeschool.onmicrosoft.com>
2021-07-06 12:54:53 +0200

    Vaadin terminado

--- src/main/java/ufv/dis/ejemplo/front/MyUI.java
+++ src/main/java/ufv/dis/ejemplo/front/MyUI.java
@@ -8,7 +8,11 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.*;
 import ufv.dis.ejemplo.back.Equipo;
+import ufv.dis.ejemplo.back.Funciones;
 import ufv.dis.ejemplo.back.Miembro;
+import ufv.dis.ejemplo.back.PWException;
+
+import java.util.ArrayList;
 
 
 @Theme("mytheme")
@@ -22,19 +26,76 @@ public class MyUI extends UI {
     final VerticalLayout eliminar = new VerticalLayout();
 
     //===============AÑADIR==================//
-    final TextField capital = new TextField("Capital: ");
-    final TextField pais = new TextField("pais: ");
-    final TextField continente = new TextField("Continente: ");
+    final TextField name = new TextField("Nombre super: ");
+    final TextField age = new TextField("Edad super: ");
+    final TextField secretIdentity = new TextField("Identidad Secreta: ");
+    final TextArea powers = new TextArea("Poderes: ");
 
     FormLayout form;
-    Grid<Localidad> listaa = new Grid<>(Localidad.class);
+    Grid<Miembro> listaa = new Grid<>(Miembro.class);
 
-    
+    //===============ELIMINAR==================//
+    ArrayList<Miembro> listaMiembros = new ArrayList<>();
+    ComboBox<Miembro> select = new ComboBox<>("Selecciona Miembro a eliminar");
+
+    Grid<Miembro> listae = new Grid<>(Miembro.class);
+
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        crear.setCaption("Añadir Super");
+        eliminar.setCaption("Eliminar Super");
 
-        
-        setContent(layout);
+        equipo = Funciones.leerJson("super.json");
+
+        //===============AÑADIR==================//
+        Button button = new Button("Guardar Localidad");
+        button.addClickListener(e -> {
+            if (name.getValue().equals("") || age.getValue().equals("") || secretIdentity.getValue().equals("") || powers.getValue().equals(""))
+                Notification.show("ERROR: No dejes en blanco ningun campo", Notification.Type.WARNING_MESSAGE);
+            else{
+                try {
+                    String[] poderes = powers.getValue().split(",");
+
+                    miembro = new Miembro(name.getValue(), Integer.parseInt(age.getValue()), secretIdentity.getValue(), poderes);
+
+                    Notification.show("Localidad añadida correctamente", Notification.Type.TRAY_NOTIFICATION);
+                    equipo.crearMiembro(equipo, miembro);
+
+                } catch (PWException error) {
+                    Notification.show("ERROR: Datos mal introducidos", Notification.Type.WARNING_MESSAGE);
+                }
+                listaa.setItems(equipo.getMembers());
+            }
+        });
+
+        listaa.setItems(equipo.getMembers());
+        form = new FormLayout(name, age, secretIdentity, powers);
+        crear.addComponents(form, listaa);
+        crear.setMargin(true);
+
+        //===============COMBO BOX==================//
+        listaMiembros = equipo.getMembers();
+        select.setItems(listaMiembros);
+
+        select.setTextInputAllowed(false);
+        select.setEmptySelectionAllowed(false);
+        select.setEmptySelectionCaption("Selecciona un valor");
+
+        select.setItemCaptionGenerator(Miembro::getName);
+
+        select.addValueChangeListener(event->{
+            equipo.eliminarMiembro(equipo, select.getValue());
+            listae.setItems(equipo.getMembers());
+        });
+
+        listae.setItems(equipo.getMembers());
+
+        eliminar.addComponents(select, listae);
+
+
+        tabSheet.addComponents(crear, eliminar);
+        setContent(tabSheet);
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
